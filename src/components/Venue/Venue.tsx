876288980eb37add3e906e9e8b7e8dfd50e86da7
Corey01 <corey.obeirne@gmail.com>
2025-05-25 23:53:40 +0100

    working on venue page

--- src/components/Venue/Venue.tsx
+++ src/components/Venue/Venue.tsx
@@ -1,8 +1,17 @@
+import style from './Venue.module.scss';
+
 const Venue = () => {
     return (
-        <div>
-        <h1>Venue</h1>
-        <p>Our special day will be hosted at Falcon Manor in Settle, in the Yorkshire Dales.</p>
+        <div className="content">
+        <h2>Venue</h2>
+        <div className={style.body}>
+            <div className={style.copy}>
+            <p>Our special day will be hosted at <span>Falcon Manor</span> in Settle, in the Yorkshire Dales.</p>
+
+            <p>Settle is a small, friendly market town in the Yorkshire Dales, surrounded by beautiful countryside and dramatic limestone hills. With its stone buildings, independent shops, and weekly market, it has a warm, relaxed atmosphere – the perfect place to slow down, explore, and enjoy the landscape.</p>
+            </div>
+            <img src="/venue.png" alt="Falcon Manor" />
+        </div>
         </div>
     );
 }
